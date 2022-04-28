638bbb833d1c460e9e058d088e527d3f576402c6
Dustin Firebaugh <dafirebaugh@gmail.com>
2022-04-27 23:30:33 -0400

    simplifying the chatservice server implementation

--- internal/services/chatservice/server.go
+++ internal/services/chatservice/server.go
@@ -2,9 +2,7 @@ package chatservice
 
 import (
 	"cheezewiz/internal/models/message"
-	"cheezewiz/pkg/broker"
 	"context"
-	"sync"
 
 	flatbuffers "github.com/google/flatbuffers/go"
 	"github.com/sirupsen/logrus"
@@ -12,87 +10,42 @@ import (
 
 const Port = 10578
 
-type streamHandler struct {
-	wg     *sync.WaitGroup
-	stream message.MessageHandler_SubscribeMessageServer
-}
-
 type chatServer struct {
-	streams    map[string][]streamHandler
-	chatBroker *broker.Broker
+	listeners []message.MessageHandler_SubscribeMessageServer
 }
 
 func NewServer() *chatServer {
-	b := broker.New()
-	go b.Start()
 	return &chatServer{
-		streams:    make(map[string][]streamHandler),
-		chatBroker: b,
+		listeners: []message.MessageHandler_SubscribeMessageServer{},
 	}
 }
 
 func (c *chatServer) PublishMessage(ctx context.Context, request *message.MessageRequest) (*flatbuffers.Builder, error) {
 	logrus.Debugf("publishing message: [%s->%s]: %s", string(request.Source()), string(request.Destination()), string(request.Body()))
 
-	for _, s := range c.streams[string(request.Destination())] {
-		s.wg.Add(1)
-	}
+	for _, s := range c.listeners {
+		err := s.Send(buildMessageFromRequest(request))
 
-	go c.chatBroker.Publish(brokerMessage{
-		destination: string(request.Destination()),
-		source:      string(request.Source()),
-		message:     string(request.Body()),
-	})
+		if err != nil {
+			c.removeListener(s)
+		}
+	}
 	return buildMessageFromRequest(request), nil
-}
-
-func (c *chatServer) registerListener(destination string, stream message.MessageHandler_SubscribeMessageServer) {
-	logrus.Infof("registering listener for: %s", destination)
-	_, exists := c.streams[destination]
-	if !exists {
-		wg := &sync.WaitGroup{}
-		wg.Add(1)
-		c.streams[destination] = []streamHandler{{
-			wg:     wg,
-			stream: stream,
-		}}
-		return
-	}
-
-	sh := streamHandler{
-		wg:     &sync.WaitGroup{},
-		stream: stream,
-	}
-	sh.wg.Add(1)
-
-	c.streams[destination] = append(c.streams[destination], sh)
 }
 
 func (c *chatServer) SubscribeMessage(request *message.MessageRequest, stream message.MessageHandler_SubscribeMessageServer) error {
 	logrus.Debugf("rcvd a sub to: [%s] from [%s]", string(request.Destination()), string(request.Source()))
 
-	c.registerListener(string(request.Destination()), stream)
-	go c.listen()
+	c.addListener(stream)
+	<-stream.Context().Done()
 
-	for _, s := range c.streams[string(request.Destination())] {
-		s.wg.Wait()
-	}
-
-	return nil
+	return stream.Context().Err()
 }
 
-func (c *chatServer) listen() {
-	logrus.Debug("chat service running")
+func (c *chatServer) addListener(stream message.MessageHandler_SubscribeMessageServer) {
+	c.listeners = append(c.listeners, stream)
+}
 
-	msg := c.chatBroker.Subscribe()
-
-	for {
-		m := <-msg
-		// defer mbx.wg.Done()
-		logrus.Infof("[mailbox] sending: [%s->%s]: %s", m.GetRequestor(), m.GetTopic(), m.GetPayload())
-		builtMsg := buildMessage(m.GetRequestor(), m.GetTopic(), m.GetPayload().(string))
-		for _, s := range c.streams[m.GetTopic()] {
-			s.stream.Send(builtMsg)
-		}
-	}
+func (c *chatServer) removeListener(stream message.MessageHandler_SubscribeMessageServer) {
+	c.listeners = append(c.listeners, stream)
 }
