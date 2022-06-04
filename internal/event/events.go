4469280b098c5d8ff85f6c1da55c40bead074df0
Elias Woods <EliasWoods@gmail.com>
2022-06-04 12:49:10 -0400

    Implement spawner code

--- internal/event/events.go
+++ internal/event/events.go
@@ -1,7 +1,16 @@
 package event
 
 import (
+	"cheezewiz/internal/component"
+	"cheezewiz/internal/entity"
+	"math"
+	"math/rand"
+	"strconv"
+
+	"github.com/atedja/go-vector"
 	"github.com/yohamta/donburi"
+	"github.com/yohamta/donburi/filter"
+	"github.com/yohamta/donburi/query"
 )
 
 type Job struct {
@@ -11,7 +20,60 @@ type Job struct {
 }
 
 func spawnWave(w donburi.World, args []string) {
+	playerQuery := query.NewQuery(filter.Contains(entity.PlayerTag))
 
+	firstplayer, _ := playerQuery.FirstEntity()
+
+	pPos := component.GetPosition(firstplayer)
+	playerVector := vector.NewWithValues([]float64{pPos.X, pPos.Y})
+
+	amount, _ := strconv.Atoi(args[1])
+	hp, _ := strconv.Atoi(args[2])
+	distance := 200
+
+	radians_spread := (2.0 * math.Pi) / float64(amount)
+
+	switch {
+	case "radish" == args[0]:
+		for i := 0; i < amount; i++ {
+			x := math.Cos(radians_spread * float64(i))
+			y := math.Sin(radians_spread * float64(i))
+			spawnloc := vector.NewWithValues([]float64{x, y})
+			spawnloc.Scale(float64(distance))
+			spawnloc = vector.Add(spawnloc, playerVector)
+			e := entity.MakeEnemy(w, spawnloc[0], spawnloc[1])
+			component.GetHealth(e).HP = float64(hp)
+		}
+
+	default:
+		return
+	}
+}
+func spawnBoss(w donburi.World, args []string) {
+
+	hp, _ := strconv.Atoi(args[1])
+	distance, _ := strconv.Atoi(args[2])
+	loc_radian := rand.Float64() * (math.Pi * 2)
+
+	x := math.Cos(loc_radian)
+	y := math.Sin(loc_radian)
+
+	v := vector.NewWithValues([]float64{x, y})
+
+	v.Scale(float64(distance))
+
+	playerQuery := query.NewQuery(filter.Contains(entity.PlayerTag))
+
+	firstplayer, _ := playerQuery.FirstEntity(w)
+
+	pPos := component.GetPosition(firstplayer)
+	playerVector := vector.NewWithValues([]float64{pPos.X, pPos.Y})
+
+	v = vector.Add(playerVector, v)
+
+	e := entity.MakeEnemy(w, v[0], v[1])
+	component.GetHealth(e).HP = float64(hp)
+	//args[]
 }
 
 type JobName string
