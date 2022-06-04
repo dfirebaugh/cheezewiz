493008a9f75beb2d9439b0855e892257f3da387c
Elias Woods <EliasWoods@gmail.com>
2022-06-04 19:44:17 -0400

    Add cheezeboss

--- internal/entity/enemy.go
+++ internal/entity/enemy.go
@@ -38,3 +38,27 @@ func MakeEnemy(w donburi.World, x float64, y float64) *donburi.Entry {
 
 	return entry
 }
+
+func MakeBossEnemy(w donburi.World, x float64, y float64, hp float64) *donburi.Entry {
+	b := w.Create(EnemyTag, component.Position, component.Health, component.SpriteSheet, component.Animation)
+	entry := w.Entry(b)
+	position := (*component.PositionData)(entry.Component(component.Position))
+
+	animation := (*component.AnimationData)(entry.Component(component.Animation))
+
+	health := (*component.HealthAspect)(entry.Component(component.Health))
+
+	health.HP = 10
+
+	position.X = x
+	position.Y = y
+
+	imgDecoded, _, _ := image.Decode(bytes.NewReader(assets.CheezeBossRaw))
+
+	grid := ganim8.NewGrid(int(constant.SpriteSize*2), int(constant.SpriteSize*2), imgDecoded.Bounds().Dx(), imgDecoded.Bounds().Dy())
+
+	animation.Walk.Sprite = ganim8.NewSprite(ebiten.NewImageFromImage(imgDecoded), grid.GetFrames("1-5", 1))
+	animation.Walk.Animation = ganim8.NewAnimation(animation.Walk.Sprite, 100*time.Millisecond, ganim8.Nop)
+
+	return entry
+}
