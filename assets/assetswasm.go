530a81725ebccf9c9090d92c42b4011218b9a276
Dustin Firebaugh <dafirebaugh@gmail.com>
2022-06-08 10:54:26 -0400

    adding build flags to assets so that we have some working demo on the web

--- assets/assetswasm.go
+++ assets/assetswasm.go
@@ -1,12 +1,11 @@
-//go:build !js
-// +build !js
+//go:build js
+// +build js
 
 package assets
 
 import (
 	"bytes"
 	"cheezewiz/assets/scenes/garden"
-	"cheezewiz/assets/scenes/kitchen"
 	"embed"
 	_ "embed"
 
@@ -22,6 +21,9 @@ var AssetFS embed.FS
 func GetGardenMap() (*tiled.Map, error) {
 	return tiled.LoadReader("./assets/scenes/garden", bytes.NewReader(garden.GardenRaw))
 }
+
+// we need to embed the map assets.  However, there is currently an issue preventing us from doing that
+//   see: https://github.com/lafriks/go-tiled/issues/63
 func GetKitchenMap() (*tiled.Map, error) {
-	return tiled.LoadReader("./assets/scenes/kitchen", bytes.NewReader(kitchen.Kitchen1Raw))
+	return &tiled.Map{}, nil
 }
